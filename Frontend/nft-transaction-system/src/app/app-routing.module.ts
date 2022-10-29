7ddc82b9b0131245ae8abfacf3310569f2dad82f
Shreeprasad Sonar <65328555+ShreeprasadSonar@users.noreply.github.com>
2022-10-28 20:51:52 -0500

    added signup page
    Functionalities needs to be implemented

--- Frontend/nft-transaction-system/src/app/app-routing.module.ts
+++ Frontend/nft-transaction-system/src/app/app-routing.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
+import { AuthService } from './auth.service';
+
 import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { AuthGuard } from './auth.guard';
-import { AuthService } from './auth.service';
+import { SignupComponent } from './signup/signup.component';
+
 
 const routes: Routes = [
   { path: '', component: HomepageComponent},
   { path: 'homepage', component: HomepageComponent},
   { path: 'login', component: LoginComponent},
-  { path: 'dashboard', component: DashboardComponent, canActivate:[AuthGuard]}
+  { path: 'dashboard', component: DashboardComponent, canActivate:[AuthGuard]},
+  { path: 'signup', component: SignupComponent},
 ];
 
 @NgModule({
---
+++ Frontend/nft-transaction-system/src/app/signup/signup.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl,FormGroup,Validators,FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from './../auth.service';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
+})
+export class SignupComponent implements OnInit {
+  cstatus = 'None'
+  registerForm:any = FormGroup;
+  submitted = false;
+  wrongPassword = false;
+  constructor(private formBuilder: FormBuilder,
+              private authService: AuthService,
+              private router: Router) { }
+  
+  get f() { return this.registerForm.controls; }
+
+  ngOnInit(): void {
+    if(localStorage.getItem('isloggedIn') == 'true'){
+      this.router.navigateByUrl('/')
+    }
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+      });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+        return;
+    }
+    //True if all the fields are filled
+    if(this.submitted)
+    {
+      // this.authService.login(this.registerForm.value.email, this.registerForm.value.password).subscribe(() => {
+      //                   console.log("User is logged in");
+      //                   this.router.navigateByUrl('/');
+      //               }
+      //           );
+      this.cstatus = this.authService.login(this.registerForm.value.email, this.registerForm.value.password)
+      if(this.cstatus != 'loggedIn'){
+        this.wrongPassword = true;
+        return;
+      }
+      this.ngOnInit()
+      alert("Great!!");
+    }
+   
+  }
+
+}
