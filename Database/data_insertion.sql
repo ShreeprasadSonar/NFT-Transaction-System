f60b5f1ee0fa8aa8471df959b0d5d66e8da1bd53
Varun Potluri <96263823+varunpotluri8@users.noreply.github.com>
2022-11-09 23:33:08 -0600

    updates to Manager table

--- Database/data_insertion.sql
+++ Database/data_insertion.sql
@@ -1,6 +1,6 @@
 USE nft_system;
-INSERT INTO MANANGER (M_id ,NAME ,password) VALUES ('22224','admin','admin@123');
-INSERT INTO MANANGER (M_id ,NAME ,password) VALUES ('22225','admin1','admin@123');
+INSERT INTO MANAGER (M_id ,Email ,password) VALUES ('22224','admin@gmail.com','admin@123');
+INSERT INTO MANAGER (M_id ,Email ,password) VALUES ('22225','admin1@gmail.com','admin@123');
 INSERT INTO TRADER (t_id, t_name,t_lastname,pass,fiat_amt,Ph_no,cell_no,email,mem_type,eth_add,eth_cnt) VALUES('12346','Varun','Potluri','user@123','100.000','7372802224','7372802224','user@gmail.com','Gold','0x3db763bbbb1ac900eb2eb8b106218f85f9f64a13','5');
 INSERT INTO CLASSIFICATION (mem_id , com_rate) VALUES('GOLD','5.25');
 INSERT INTO CLASSIFICATION (mem_id , com_rate) VALUES('SILVER','7.25');
--- Database/test.sql
+++ Database/test.sql
@@ -1,1 +1,5 @@
-CREATE DATABASE test1;
+USE nft_system;
+DROP TABLE MANANGER;
+CREATE TABLE MANAGER(M_id varchar(10),Email varchar(50),password varchar(10), Primary KEY (m_id));
+INSERT INTO MANAGER (M_id ,Email ,password) VALUES ('22224','admin@gmail.com','admin@123');
+INSERT INTO MANAGER (M_id ,Email ,password) VALUES ('22225','admin1@gmail.com','admin@123');
