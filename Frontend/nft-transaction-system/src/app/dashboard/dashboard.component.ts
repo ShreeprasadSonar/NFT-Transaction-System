fb1718224f88655d948d874a2bbd26f15329fbbf
usatishvarma <48093422+usatishvarma@users.noreply.github.com>
2022-11-14 21:39:33 -0600

    wrote the api calls in dataservices for the dashboard

--- Frontend/nft-transaction-system/src/app/dashboard/dashboard.component.ts
+++ Frontend/nft-transaction-system/src/app/dashboard/dashboard.component.ts
@@ -3,12 +3,13 @@ import { AuthService } from '../auth.service';
 import { DataService } from '../data.service';
 import { NgModule } from '@angular/core';
 import UsersJson from './usernft.json';
+import { Router } from '@angular/router';
 
 interface USERS{
   Name:String;
   NFT_Address:String;
   NFT_Value:Number;
-
+  Image_URL:String
 }
 
 
@@ -19,14 +20,32 @@ interface USERS{
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  Users:USERS[]=UsersJson;
+  //Users:USERS[]=UsersJson;
 
-  constructor(private data:DataService) { }
+  trade_nfts:any;
+  add_money:any;
 
+  public searchFilter: any = '';
+  searchText = '';
+
+  constructor(private dataService: DataService,
+              private router: Router ) { }
+  
   ngOnInit(): void {
-    this.data.addEth().subscribe((data:any) => {
+    this.dataService.addEth().subscribe((data:any) => {
       console.log(data);
     });
+
+    this.dataService.getTraderNfts().subscribe((data:any) => {
+      console.log(this.trade_nfts);
+    });
+
+    this.dataService.transaction().subscribe((data:any) =>{
+
+      this.add_money=data.message;
+      console.log(this.add_money)
+    })
+
   }
 
 }
--- Frontend/nft-transaction-system/src/app/data.service.ts
+++ Frontend/nft-transaction-system/src/app/data.service.ts
@@ -28,5 +28,30 @@ export class DataService {
     });  
     return this.http.get('http://127.0.0.1:5000/transaction', { headers: headers });
   }
+//example
+  getTraderNfts(){
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+
+    });  
+    return this.http.get('http://127.0.0.1:5000/getTraderNfts', { headers: headers });
+  }
+
+  //example2
+
+  transaction(){
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+      amount: "10",
+      type: "ETH",
+      t_id: "897982743423423",
+      t_date_time: "25/06/2022 12 PM",
+      status: "Active"
+    });  
+    return this.http.get('http://127.0.0.1:5000/transaction', { headers: headers });
+  }
+
 
 }
