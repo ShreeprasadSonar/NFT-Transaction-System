ff9d4f0e55c2bdd230c66bfe5843bf7170bc7377
Shreeprasad Sonar <65328555+ShreeprasadSonar@users.noreply.github.com>
2022-11-14 23:16:49 -0600

    Revert "Update data.service.ts"
    This reverts commit 863bdec6e83fe9e0fb54610977e31f305e13849a.

--- Frontend/nft-transaction-system/src/app/data.service.ts
+++ Frontend/nft-transaction-system/src/app/data.service.ts
@@ -26,7 +26,7 @@ export class DataService {
       t_date_time: "25/06/2022 12 PM",
       status: "Active"
     });  
-    return this.http.get('http://127.0.0.1:5000/transaction', { headers: headers, email: localStorage.getItem('email') });
+    return this.http.get('http://127.0.0.1:5000/transaction', { headers: headers });
   }
 //example
   getTraderNfts(){
