a7dce84a6f3abebb22eb27565b60583dbf7266ab
usatishvarma <48093422+usatishvarma@users.noreply.github.com>
2022-11-20 15:03:05 -0600

    buynft and cancel pages

--- Frontend/nft-transaction-system/src/app/app-routing.module.ts
+++ Frontend/nft-transaction-system/src/app/app-routing.module.ts
@@ -13,22 +13,24 @@ import { SellNftsComponent } from './sell-nfts/sell-nfts.component';
 import { TransactionsComponent } from './transactions/transactions.component';
 import { AddmoreComponent } from './addmore/addmore.component';
 import { CancelComponent } from './cancel/cancel.component';
+import { BuynftComponent } from './buynft/buynft.component';
 
 
 const routes: Routes = [
   { path: '', component: HomepageComponent},
   { path: 'homepage', component: HomepageComponent},
   { path: 'login', component: LoginComponent},
-  { path: 'dashboard', component: DashboardComponent, canActivate:[AuthGuard]},
-  //{ path: 'dashboard', component: DashboardComponent},
+  //{ path: 'dashboard', component: DashboardComponent, canActivate:[AuthGuard]},
+  { path: 'dashboard', component: DashboardComponent},
   { path: 'signup', component: SignupComponent},
-  { path: 'trade', component: TradeComponent, canActivate:[AuthGuard]},
-  //{ path: 'trade', component: TradeComponent},
+  //{ path: 'trade', component: TradeComponent, canActivate:[AuthGuard]},
+  { path: 'trade', component: TradeComponent},
   { path: 'manager-dashboard', component: ManagerDashboardComponent},
   { path: 'sell-nfts', component: SellNftsComponent},
   { path: 'transactions', component: TransactionsComponent},
   {path:'addmore',component:AddmoreComponent},
   {path:'cancel',component:CancelComponent},
+  {path:'buynft',component:BuynftComponent},
 ];
 
 @NgModule({
