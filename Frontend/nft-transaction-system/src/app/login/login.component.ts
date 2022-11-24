9d5ae5097ef2e0a0760447fd375c6e18c9d99d1c
Shreeprasad Sonar <65328555+ShreeprasadSonar@users.noreply.github.com>
2022-11-23 20:16:37 -0600

    Login logic change

--- Frontend/nft-transaction-system/src/app/login/login.component.ts
+++ Frontend/nft-transaction-system/src/app/login/login.component.ts
@@ -14,6 +14,7 @@ export class LoginComponent implements OnInit {
   submitted = false;
   wrongPassword = false;
   response:any;
+  message:string = "";
 
   constructor(private formBuilder: FormBuilder,
               private authService: AuthService,
@@ -29,37 +30,39 @@ export class LoginComponent implements OnInit {
     //True if all the fields are filled
     if(this.submitted)
     {
-      // this.authService.login(this.registerForm.value.email, this.registerForm.value.password).subscribe(() => {
-      //                   console.log("User is logged in");
-      //                   this.router.navigateByUrl('/');
-      //               }
-      //           );
-      // this.cstatus = this.authService.login(this.registerForm.value.email, this.registerForm.value.password)
       if (this.registerForm.value.email && this.registerForm.value.password && this.registerForm.value.type) {
         this.authService.login(this.registerForm.value.email, this.registerForm.value.password, this.registerForm.value.type)
             .subscribe(
                 res => {
                     this.response = res
-                    console.log("User is logged in");
-                    localStorage.setItem('isloggedIn', 'true');
-                    localStorage.setItem('token', this.response.auth_token);
-                    localStorage.setItem('email', this.registerForm.value.email);
-                    localStorage.setItem('userType', this.registerForm.value.type);
-                    localStorage.setItem('id', this.response.id);
-                    if(this.registerForm.value.type == 'Manager'){
-                      this.router.navigateByUrl('/manager-dashboard');
-                    } else{
-                      this.router.navigateByUrl('/dashboard');
+                    console.log(this.response)
+                    if(this.response.status != 'fail'){
+                      localStorage.setItem('isloggedIn', 'true');
+                      localStorage.setItem('token', this.response.token);
+                      localStorage.setItem('type', this.response.type);
+                      localStorage.setItem('email', this.response.email);
+                      localStorage.setItem('name', this.response.name);
+                      localStorage.setItem('id', this.response.id);
+                      if(this.registerForm.value.type == 'Manager'){
+                        this.router.navigateByUrl('/manager-dashboard');
+                      } else{
+                        this.router.navigateByUrl('/dashboard');
+                      }
+                      alert("Great!!");
+                      
+                    } else {
+                      this.message = this.response.message
+                      this.wrongPassword = true;
+                      console.log("User is not logged in");
                     }
+                },
+                error =>{
+                  this.message = "Error while logging in"
+                  this.wrongPassword = true;
+                  console.log("User is not logged in");
                 }
             );
-    }
-      if(this.cstatus != 'loggedIn'){
-        this.wrongPassword = true;
-        return;
       }
-      this.ngOnInit()
-      alert("Great!!");
     }
    
   }
